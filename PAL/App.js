5d79ebde6aa1e273244569dfd3851f9e6fe9b2a4
Da Yeon Hwang <dahwang2018@u.northwestern.edu>
2018-05-10 23:27:14 -0500

    added detail sections on dashboard

--- PAL/App.js
+++ PAL/App.js
@@ -1,22 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, FlatList } from 'react-native';
+import Dimensions from 'Dimensions';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>PAL</Text>
-        <Text>By Orange Team</Text>
-      </View>
+      <FlatList
+        data={data}
+        renderItem={({item}) => (
+          <View style={styles.itemContainer}>
+            <Icon style={styles.searchIcon} name={item.icon} size={20} color="#000" />
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.id}
+        numColumns={numColumns} />
     );
   }
 }
 
+const data = [
+  {id: 1, name: 'Message', icon: 'comments'},
+  {id: 2, name: 'So Far Today', icon: 'bar-chart'},
+  {id: 3, name: 'Share', icon:'share'},
+  {id: 4, name: 'Heart Rate', icon:'heart'},
+  {id: 5, name: 'Send a Hug or Calming Technique'},
+  {id: 6, name: 'Body Temperature', icon: 'thermometer-0'},
+];
+const numColumns = 3;
+const size = Dimensions.get('window').width/numColumns;
 const styles = StyleSheet.create({
-  container: {
+  itemContainer: {
+    width: size,
+    height: size,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    top: 400,
+  },
+  item: {
+    flex: 1,
+    textAlign: 'center',
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 0,
+    backgroundColor: '#fff',
+    color: '#424242',
+  },
+  searchIcon: {
+    padding: 10,
   },
 });
