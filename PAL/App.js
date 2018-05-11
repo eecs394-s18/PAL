2280846c0962397d35e3d48a72cf1f4c155ea8a7
LYZ <o4y1j1@u.northwestern.edu>
2018-05-11 14:14:39 -0500

    dayeon's modification

--- PAL/App.js
+++ PAL/App.js
@@ -1,22 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, FlatList } from 'react-native';
+import Dimensions from 'Dimensions';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>PAL</Text>
-        <Text>By Orange Team</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <FlatList
+                data={data}
+                renderItem={({item}) => (
+                    <View style={styles.itemContainer}>
+                        <Icon style={styles.searchIcon} name={item.icon} size={20} color="#000" />
+                        <Text style={styles.item}>{item.name}</Text>
+                    </View>
+                )}
+                keyExtractor={item => item.id}
+                numColumns={numColumns} />
+        );
+    }
 }
 
+const data = [
+    {id: 1, name: 'Message', icon: 'comments'},
+    {id: 2, name: 'So Far Today', icon: 'bar-chart'},
+    {id: 3, name: 'Share', icon:'share'},
+    {id: 4, name: 'Heart Rate', icon:'heart'},
+    {id: 5, name: 'Send a Hug or Calming Technique'},
+    {id: 6, name: 'Body Temperature', icon: 'thermometer-0'},
+];
+const numColumns = 3;
+const size = Dimensions.get('window').width/numColumns;
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    itemContainer: {
+        width: size,
+        height: size,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        top: 400,
+    },
+    item: {
+        flex: 1,
+        textAlign: 'center',
+        paddingTop: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingLeft: 0,
+        backgroundColor: '#fff',
+        color: '#424242',
+        marginRight: 10,
+        marginLeft: 10,
+    },
+    searchIcon: {
+        padding: 10,
+    },
 });
