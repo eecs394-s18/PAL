717cb107a3350561a2380d67584b1ddca29be54a
Benjamin Kalish <bkalish@rocketvisor.com>
2018-05-17 19:28:35 -0500

    added chart to Reports tab via Victory charts and fixed the calendar

--- PAL/Report.js
+++ PAL/Report.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { TouchableHighlight, StyleSheet, Text, View, StatusBar, FlatList, Alert, Image} from 'react-native';
 import Dimensions from 'Dimensions';
+import { VictoryChart, VictoryLine, VictoryTheme } from "victory-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Header, Button } from 'react-native-elements';
@@ -17,16 +18,59 @@ export default class ReportsScreen extends React.Component {
 
   render() {
     return (
-        <CalendarStrip
-            calendarAnimation={{type: 'sequence', duration: 30}}
-            daySelectionAnimation={{type: 'background', duration: 300, highlightColor: '#9265DC'}}
-            style={{height:140, paddingTop: 40, paddingBottom: 5}}
-            calendarHeaderStyle={{color: 'white'}}
-            calendarColor={'#7743CE'}
-            dateNumberStyle={{color: 'white'}}
-            dateNameStyle={{color: 'white'}}
-            iconContainer={{flex: 0.1}}
-        />
+        <View>
+            <Header
+            placement="left"
+            backgroundColor = "#ff1900"
+            leftComponent={< ShirtStatus />}
+            centerComponent={{ text: 'PAL', style: {color: '#fff', marginLeft: -30} }}
+            rightComponent={{ icon: 'menu', color: '#fff' }}
+            />
+
+          <CalendarStrip
+              calendarAnimation={{type: 'sequence', duration: 30}}
+              daySelectionAnimation={{type: 'background', duration: 300, highlightColor: '#9265DC'}}
+              style={{height:100, paddingTop: 5, paddingBottom: 5}}
+              calendarHeaderStyle={{color: 'white'}}
+              calendarColor={'#7743CE'}
+              dateNumberStyle={{color: 'white'}}
+              dateNameStyle={{color: 'white'}}
+              iconContainer={{flex: 0.1}}
+          />
+          <VictoryChart
+            theme={VictoryTheme.material}
+          >
+            <VictoryLine
+              style={{
+                data: { stroke: "#2082d8" },
+                parent: { border: "1px solid #ccc"}
+              }}
+              data={[
+                { x: 1, y: 2 },
+                { x: 2, y: 3 },
+                { x: 3, y: 5 },
+                { x: 4, y: 4 },
+                { x: 5, y: 7 }
+              ]}
+            />
+          </VictoryChart>
+        </View>
+
+
     );
   }
 }
+
+class ShirtStatus extends React.Component{
+  render() {
+    return (
+      <Text>
+        <Image
+          source={require('./resources/tshirt_white.png')}
+          style= {{height: 20, width: 20}}
+        />
+        <Icon name={"battery-three-quarters"} size={20} color="#fff"/>
+      </Text>
+    );
+  }
+};
