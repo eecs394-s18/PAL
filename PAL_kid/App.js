446e428ec429edd3928afe072904d7f4335e8d77
Kevin Mui <kevinmui2020@u.northwestern.edu>
2018-06-05 12:12:43 -0500

    Change child app

--- PAL_kid/App.js
+++ PAL_kid/App.js
@@ -15,8 +15,11 @@ import styles from './styles/home';
 import MyCircleScreen from './MyCircle';
 import ReportsScreen from './Report';
 import { GetGradient } from './gradient';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 import { Constants, Location, Permissions, MapView } from 'expo';
+
+import { openMap } from 'react-native-open-map';
 
 //var txtFile = "c:/test.txt"
 //var file = new File(txtFile);
@@ -51,6 +54,7 @@ class HomeScreen extends React.Component {
 	    lat: "",
 	    lng: "",
 	    locationName: "",
+      items: {},
 
 	    mapRegion: {
 	    	latitude: 37.78825,
@@ -74,7 +78,7 @@ class HomeScreen extends React.Component {
 		//setInterval( this._timer, 500);
 	}
 
-    _timer() { 
+    _timer() {
 	       let d = new Date();
 	       let result = d.getHours() + d.getMinutes() / 60;
 	       this.setState({
@@ -172,6 +176,60 @@ class HomeScreen extends React.Component {
 	    batfirebase.on('value', snapshot => {this.setState({battery: snapshot.val()})
 	    });
 	}
+
+  getDirections = () => {
+    openMap({
+      latitude: this.state.lat,
+      longitude: this.state.lng,
+    });
+  }
+
+  loadItems(day) {
+   setTimeout(() => {
+     var strTime;
+     var time;
+     for (let i = 0; i < 1; i++) {
+       time = day.timestamp;
+       strTime = this.timeToString(time);
+       if (!this.state.items[strTime]) {
+         this.state.items[strTime] = [];
+         const numItems = Math.floor(Math.random() * 5);
+           for (let j = 0; j < numItems; j++) {
+             this.state.items[strTime].push({
+               name: 'Item for ' + strTime,
+               height: Math.max(50, Math.floor(Math.random() * 150))
+             });
+         }
+
+       }
+     }
+
+     const newItems = {};
+     Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+     this.setState({
+       items: newItems
+     });
+   }, 100);
+
+
+ }
+ renderItem(item) {
+   return (
+     <View style={[sched_styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+   );
+ }
+ renderEmptyDate() {
+   return (
+     <View style={sched_styles.emptyDate}><Text>This is empty date!</Text></View>
+   );
+ }
+ rowHasChanged(r1, r2) {
+   return r1.name !== r2.name;
+ }
+ timeToString(time) {
+   const date = new Date(time);
+   return date.toISOString().split('T')[0];
+ }
    render() {
 
     return (
@@ -207,10 +265,13 @@ class HomeScreen extends React.Component {
 		            </View>
 
 	               <View style={{ justifyContent: 'center', top: 175, height: 75, backgroundColor: '#e4e4e4'}}>
-	                    <Text style = {{textAlign: 'center'}}> You are at {this.state.locationName}</Text>
+	                    <Text onPress={this.getDirections} style = {{textAlign: 'center'}}>
+                        You are at
+                        <Text style={{fontWeight: "bold"}}> {this.state.locationName}</Text>
+                      </Text>
 	  				</View>
 
-	  				<View>
+            <View>
 	  					<MapView
 				          style={styles.map}
 				          region={this.state.mapRegion}
@@ -221,6 +282,13 @@ class HomeScreen extends React.Component {
 	          </View>
           </View>
         </View>
+        // <Agenda
+        //   items={this.state.items}
+        //   loadItemsForMonth={this.loadItems.bind(this)}
+        //   renderItem={this.renderItem.bind(this)}
+        //   renderEmptyDate={this.renderEmptyDate.bind(this)}
+        //   rowHasChanged={this.rowHasChanged.bind(this)}
+        // />
     );
   }
 }
@@ -240,12 +308,27 @@ class ShirtStatus extends React.Component{
   }
 };
 
+const sched_styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
+  }
+});
+
 
 export default createBottomTabNavigator(
   {
     Home: HomeScreen,
-    Report: ReportsScreen,
-    MyCircle: MyCircleScreen,
+    Schedule: MyCircleScreen,
   },
   {
     navigationOptions: ({ navigation }) => ({
@@ -257,7 +340,7 @@ export default createBottomTabNavigator(
         } else if (routeName === 'Report') {
           iconName = `ios-options${focused ? '' : '-outline'}`;
         }
-        else if (routeName === 'MyCircle') {
+        else if (routeName === 'Schedule') {
           iconName = `ios-people${focused ? '' : '-outline'}`;
         }
         return <Ionicons name={iconName} size={25} color={tintColor} />;
--- PAL_kid/MyCircle.js
+++ PAL_kid/MyCircle.js
@@ -1,26 +1,125 @@
 import React, { Component } from 'react';
-import { TouchableHighlight, StyleSheet, Text, View, StatusBar, FlatList, Alert, Image} from 'react-native';
-import Dimensions from 'Dimensions';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { Header, Button } from 'react-native-elements';
-import { createBottomTabNavigator } from 'react-navigation';
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
-import CalendarStrip from 'react-native-calendar-strip';
-import moment from 'moment';
-import * as firebase from 'firebase';
-
-import styles from './styles/home';
+import { StyleSheet,Text,View,} from 'react-native';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 
-export default class MyCircleScreen extends React.Component {
+export default class ScheduleScreen extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      items: {}
+    };
   }
 
   render() {
     return (
-      <Text>"Hello"</Text>
+      <Agenda
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        onDayPress={this.onDayPress.bind(this)}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+      />
     );
   }
+
+
+  loadItems(day) {
+   setTimeout(() => {
+     var strTime;
+     var time;
+     for (let i = 0; i < 1; i++) {
+       time = day.timestamp;
+       strTime = this.timeToString(time);
+       if (!this.state.items[strTime]) {
+         this.state.items[strTime] = [];
+         const numItems = Math.floor(Math.random() * 5);
+           for (let j = 0; j < numItems; j++) {
+             this.state.items[strTime].push({
+               name: 'Item for ' + strTime,
+               height: Math.max(50, Math.floor(Math.random() * 150))
+             });
+         }
+
+       }
+     }
+
+     const newItems = {};
+     Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+     this.setState({
+       items: newItems
+     });
+   }, 100);
+
+
+ }
+
+   onDayPress(day) {
+    this.state.items = {}
+    setTimeout(() => {
+      var strTime;
+      var time;
+      for (let i = 0; i < 1; i++) {
+        time = day.timestamp;
+        strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+            for (let j = 0; j < numItems; j++) {
+              this.state.items[strTime].push({
+                name: 'Item for ' + strTime,
+                height: Math.max(50, Math.floor(Math.random() * 150))
+              });
+          }
+
+        }
+      }
+
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 100);
+
+
+  }
+
+ renderItem(item) {
+   return (
+     <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+   );
+ }
+
+ renderEmptyDate() {
+   return (
+     <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+   );
+ }
+
+ rowHasChanged(r1, r2) {
+   return r1.name !== r2.name;
+ }
+
+ timeToString(time) {
+   const date = new Date(time);
+   return date.toISOString().split('T')[0];
+ }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
+  }
+});
